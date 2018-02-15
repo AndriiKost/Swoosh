eef8df4e460078ab381f98ca7227e5dd04454fe6
Andrii Kost <andriikost@yahoo.com>
2018-02-15 08:35:07 -0600

    finished with passing data

---
+++ Swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Andrii Kost on 2/14/18.
+//  Copyright © 2018 Andrii Kost. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkill: String?
+}
